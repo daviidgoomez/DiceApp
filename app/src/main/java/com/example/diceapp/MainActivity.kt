82a689cb945f1a2248b8e1982ddd051dc2d36211
roler <11955735@ieselgrao.org>
2023-11-16 21:52:04 +0100

    VERSION FINAL

--- app/src/main/java/com/example/diceapp/MainActivity.kt
+++ app/src/main/java/com/example/diceapp/MainActivity.kt
@@ -2,10 +2,48 @@ package com.example.diceapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var rollButton: Button
+    private lateinit var resultadoTexto: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initComponents()
+        initListener()
     }
+
+    private fun initComponents() {
+        rollButton = findViewById(R.id.boton)
+        resultadoTexto = findViewById(R.id.texto)
+    }
+
+    private fun initListener() {
+        rollButton.setOnClickListener { rollDice()}
+    }
+
+
+
+    private fun rollDice(){
+        val dice = Dice(6)
+        val diceRoll = dice.roll()
+
+        resultadoTexto.text = diceRoll.toString()
+    }
+
+
+
+    class Dice(private val numSides:Int) {
+        fun roll():Int {
+            return (1 ..numSides).random()
+        }
+    }
+
+
+
+
+
 }
